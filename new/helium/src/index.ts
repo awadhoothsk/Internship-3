dfc9f09125391649217d700efaef4a8df6ba9ea6
awadhoothsk <awadhooths16@gmail.com>
2025-03-11 16:23:54 +0530

    file2.o

--- new/helium/src/index.ts
+++ new/helium/src/index.ts
@@ -14,18 +14,26 @@ const app = new Hono()
 //   console.log(`Server is running on http://localhost:${info.port}`)
 // })
 
-const reminder = {
-  id: '1',
-  message: 'Hello Hono!',
-  time: '2021-10-10',
-  completed: false
-}
-app.get('/reminder', (c) => {
-  return c.json(reminder, 200)  
+// const reminder = {
+//   id: '1',
+//   message: 'Hello Hono!',
+//   time: '2021-10-10',
+//   completed: false
+// }
+// app.get('/reminder', (c) => {
+//   return c.json(reminder, 200)  
+// })
+
+
+app.get('/random', (context) => {
+  let random = Math.floor(Math.random() * 100)
+  return context.json({ random }, 200)  
 })
 
-
-app.get('/health', (c) => {
-  return c.json({message:'Hello Hono!'},200)
+app.get('/time', (context) => {
+  return context.json({ time: new Date().toISOString() }, 200)
+})
+app.get('/enviroment', (context) => {
+  return context.json({ env: process.env.NODE_ENV }, 200)
 })
 serve(app);
